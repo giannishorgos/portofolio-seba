73aa8b846e2432e20e0d0a50745d9bcab469faeb
giannishorgos <giannishorgos@gmail.com>
2023-02-11 22:14:19 +0200

    first commit

---
+++ portfolio/src/Screens/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+    return (
+        <div className='home'>
+            <div className='welcome-text'>
+                <h1>Hi, I'm Ioannis Horgos.</h1>
+                <h3>Software developer. Biker. Gym Rat.</h3>
+            </div>
+            <div className='profile'> image</div>
+        </div>
+    );
+};
+
+export default Home;
