d70506f485663c5831af704cb1fcc8f15d255905
Ioannis Horgos <jhor@acta.com.gr>
2023-02-11 21:08:24 +0200

    menu component

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-import Home from './Screens/Home/Home';
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./Screens/Home/Home";
+import Menu from "./components/Menu/Menu";
 
 function App() {
     return (
-        <div className='App'>
+        <div className="App">
+            <Menu />
             <Home />
         </div>
     );
